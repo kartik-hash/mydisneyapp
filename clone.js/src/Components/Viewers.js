8f2b5892577430ddf35b23172bae7d47facd6fea
kartik-hash <arorakartik669@gmail.com>
2023-10-02 23:50:16 +0530

    Your commit message here

--- clone.js/src/Components/Viewers.js
+++ clone.js/src/Components/Viewers.js
@@ -4,32 +4,32 @@ const Viewers = (props) => {
         <Container >
             <Wrap>
                 <img src="./images/viewers-disney.png" alt="" />
-                <video autoPlay muted >
+                <video autoPlay loop muted >
           <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
         </video>
                 {/* plays inline true ka matlab videeo jaisi hai means choti hai toh vaisi hi play hogi agar inline nahi karunga toh automatic full screen mei open ho jayegi    and loop means video khtam hote hi auto play ho jayegi*/}
             </Wrap>
             <Wrap>
                 <img src="./images/viewers-pixar.png" alt="" />
-                <video autoPlay muted playsInline={true}>
+                <video autoPlay loop muted>
                     <source src="/videos/1564676714-pixar.mp4" type="video/mp4"/>
                 </video> 
             </Wrap>
             <Wrap>
                 <img src="./images/viewers-marvel.png" alt="" />
-                <video autoPlay muted>
+                <video autoPlay loop muted>
                     <source src="/videos/1564676115-marvel.mp4" type="video/mp4"/>
                 </video> 
             </Wrap>
             <Wrap>
                 <img src="./images/viewers-starwars.png" alt="" />
-                <video autoPlay muted>
+                <video autoPlay loop muted>
                     <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
                 </video> 
             </Wrap>
             <Wrap>
                 <img src="./images/viewers-national.png" alt="" />
-                <video autoPlay muted>
+                <video autoPlay loop  muted>
                     <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
                 </video> 
             </Wrap>
